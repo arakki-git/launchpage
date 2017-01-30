a41887e415784b81df458a41967c502f334957f0
root <root@ip-172-31-47-226.us-west-2.compute.internal>
2017-01-30 07:32:28 +0000

    fix countdown

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -56,25 +56,16 @@ jQuery(document).ready(function($) {
 	-------------------------------------------------------------------*/
 	$('#time_countdown').countDown({
         
-        // targetDate: {
-        //     'day': 30,
-        //     'month': 9,
-        //     'year': 2015,
-        //     'hour': 0,
-        //     'min': 0,
-        //     'sec': 0
-        // },
-        // omitWeeks: true
+         targetDate: {
+             'day': 3,
+             'month': 9,
+             'year': 2017,
+             'hour': 0,
+             'min': 0,
+             'sec': 0
+         },
+         omitWeeks: true
 
-         targetOffset: {
-            'day':      0,
-            'month':    0,
-            'year':     1,
-            'hour':     0,
-            'min':      0,
-            'sec':      3
-		},
-		omitWeeks: true
 
 	    });
 
